6865b750aba8530123ad31bb9b17f71ebf8fd86f
Khahmad1 <ahmad2003kharma@gmail.com>
2022-12-07 15:22:24 +0200

    i finesed step_8

---
+++ javascript-basics-01/step_8.js
@@ -0,0 +1,11 @@
+ document.getElementById("validate").addEventListener("click", function(){
+     var age = document.getElementById('age').value;
+     if (age > 18) {
+          alert("you are over 18");
+    }
+      else(age <18) 
+       { alert("you are under 18")}
+      
+ })
+ 
+ 
