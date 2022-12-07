19a67aa700f3d7ac7d5fa4c13efca5b70339181d
Khahmad1 <ahmad2003kharma@gmail.com>
2022-12-07 15:33:47 +0200

    i modified step_4

--- javascript-basics-01/step_4.js
+++ javascript-basics-01/step_4.js
@@ -1,7 +1,15 @@
-var name = prompt("Enter your name")
-var surname = prompt("Enter your surname")
-var city = prompt ("Enter your city ")
-alert (` hello name :${name}
-surname : ${surname}
-city: ${city} 
-` )
+var name = prompt("Your name please:");
+document.getElementById("name").value = name;
+var surname = prompt("Your surname please:");
+document.getElementById("surname").value = surname;
+var city = prompt("Your city please:");
+document.getElementById("city").value = city;
+
+
+document.getElementById("validate").onclick = function() {
+    alert(`
+        Name: ${name}
+        Surname: ${surname}
+        City: ${city}
+    `)
+};
