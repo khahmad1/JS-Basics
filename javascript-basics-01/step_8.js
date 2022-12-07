9001fa4b753acdb9b1ca07cdf30311c7869cbf06
Khahmad1 <ahmad2003kharma@gmail.com>
2022-12-07 16:11:51 +0200

    i modified step_8

--- javascript-basics-01/step_8.js
+++ javascript-basics-01/step_8.js
@@ -1,11 +1,11 @@
- document.getElementById("validate").addEventListener("click", function(){
-     var age = document.getElementById('age').value;
-     if (age > 18) {
-          alert("you are over 18");
-    }
-      else(age <18) 
-       { alert("you are under 18")}
-      
- })
- 
+var Age = prompt("Your age please:")
+document.getElementById("age").value = Age;
+document.getElementById("validate").addEventListener("click", function () {
+  var age = document.getElementById("age").value;
+  if (age < 18) {
+    alert("you are under 18")
+} else{
+    alert("you are over 18");
+}
+});
  
