83f86ef2f6e207f73d081b64512c245dbaef95d5
Khahmad1 <ahmad2003kharma@gmail.com>
2022-12-07 11:16:32 +0200

    i finshed

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,7 @@
+var name = prompt("Enter your name")
+var surname = prompt("Enter your surname")
+var city = prompt ("Enter your city ")
+alert (` hello name :${name}
+surname : ${surname}
+city: ${city} 
+` )
