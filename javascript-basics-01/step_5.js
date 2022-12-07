23012409e7c60c30d00401560aee3c2913684576
Khahmad1 <ahmad2003kharma@gmail.com>
2022-12-07 11:36:18 +0200

    i finishd step_5

---
+++ javascript-basics-01/step_5.js
@@ -0,0 +1,8 @@
+var num1 = parseInt(prompt("Enter first num "));
+var num2 = parseInt(prompt("Enter second num "));
+var product= num1*num2;
+alert(`
+num1 : ${num1}
+num2 : ${num2}
+result :${product}
+`)
