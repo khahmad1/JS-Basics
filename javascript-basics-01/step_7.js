492cc58600e73c0c9e063f7456b0d204f1a4030d
Khahmad1 <ahmad2003kharma@gmail.com>
2022-12-07 16:07:31 +0200

    i modified step_7

--- javascript-basics-01/step_7.js
+++ javascript-basics-01/step_7.js
@@ -1,6 +1,8 @@
+var shoeSize = prompt("Your hsoe size please");
+document.getElementById("shoe_size").value = shoeSize;
+var year = prompt("Your year age please:");
+document.getElementById("year").value = year;
+var result = (shoeSize * 2 + 5)*50 - year + 1766;
 document.getElementById("validate").addEventListener("click", function() {
-    var shoeSize = document.getElementById("shoe_size").value;
-    var year = document.getElementById("year").value;
-    var result = (shoeSize * 2 + 5)*50 - year + 1766;
     alert(result);
 });
